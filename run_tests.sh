13a309c989a035d2b354ca80e4f11a3b2a0bd296
Kenneth Ceyer <kennethan@nhpcw.com>
2019-03-21 21:00:59 +0900

    update test and install scripts

--- run_tests.sh
+++ run_tests.sh
@@ -1,3 +1,19 @@
 #!/usr/bin bash 
 
-io lib/jasmineio/jasmine.io tests/main_spec.io
+JASMINE_HOME="/tmp/jasmine"
+JASMINE_BIN="$JASMINE_HOME/bin/jasmineio"
+
+TEST_TARGET_DIR="tests"
+
+if [ ! -d $JASMINE_HOME ]; then
+    git clone git@github.com:bekkopen/jasmineio.git $JASMINE_HOME
+fi
+
+for file in $TEST_TARGET_DIR/*.io;
+do
+    if [ ! -f "$file" ]; then
+        continue
+    fi
+    echo "io $JASMINE_BIN $file;"
+    io $JASMINE_BIN $file;
+done
